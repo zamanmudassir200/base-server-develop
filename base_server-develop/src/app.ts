66318180dda3537d00ebe7e8d1bb5152199b3456
zamanmudassir200 <zamanmudassir200@gmail.com>
2024-10-21 12:46:59 +0500

    few changes

--- base_server-develop/src/app.ts
+++ base_server-develop/src/app.ts
@@ -16,7 +16,7 @@ app.use(
     cors({
         methods: ['GET', 'POST', 'DELETE', 'OPTIONS', 'HEAD', 'PUT', 'PATCH'],
         origin: ['http://localhost:3001'],
-        credentials: truen
+        credentials: true
     })
 )
 app.use(express.json())
